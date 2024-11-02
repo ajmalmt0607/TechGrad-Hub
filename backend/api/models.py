5466b29df7887a35a1f105f3c982656e2cdf04ec
ajmalmt0607 <ajmalmt67@gmail.com>
2024-11-02 18:50:22 +0530

    variant and variant_item model created

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,172 @@
+import math
 from django.db import models
+from django.utils.text import slugify
+from django.utils import timezone
 
-# Create your models here.
+
+from userauths.models import User, Profile
+from shortuuid.django_fields import ShortUUIDField
+from moviepy.editor import VideoFileClip
+
+LANGUAGE = (
+    ("English", "English"),
+    ("Spanish", "Spanish"),
+    ("French", "French"),
+)
+
+LEVEL = (
+    ("Beginner", "Beginner"),
+    ("Intermediate", "Intermediate"),
+    ("Advanced", "Advanced"),
+)
+
+TEACHER_STATUS = (
+    ("Draft", "Draft"),
+    ("Disabled", "Disabled"),
+    ("Published", "Published"),
+)
+
+PLATFORM_STATUS = (
+    ("Review", "Review"),
+    ("Disabled", "Disabled"),
+    ("Rejected", "Rejected"),
+    ("Draft", "Draft"),
+    ("Published", "Published"),
+)
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="course-file", blank=True, null=True, default="default.jpg")
+    full_name = models.CharField(max_length=100)
+    bio = models.CharField(max_length=100, blank=True, null=True)
+    facebook = models.URLField(null=True, blank=True)
+    twitter = models.URLField(null=True, blank=True)
+    linkedin = models.URLField(null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
+    
+    def students(self):
+        return CartOrderItem.objects.filter(teacher=self)
+    
+    def courses(self):
+        return Course.objects.filter(teacher=self)
+
+    def review(self):
+        return Course.objects.filter(teacher=self).count()
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image  = models.FileField(upload_to="course-file", default="category.jpg", null=True, blank=True)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Category"
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+    
+    def course_count(self):
+        return Course.objects.filter(category=self).count()
+    
+    def save(self, *args, **kwargs):# rewriting default save method
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
+class Course(models.Model):
+    # when category that is related to a course gets deleted, we dont want to delete the whole course that is (on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    file = models.FileField(upload_to="course-file", blank=True, null=True)
+    image = models.FileField(upload_to="course-file", blank=True, null=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    language = models.CharField(choices=LANGUAGE, default="English")
+    level = models.CharField(choices=LEVEL, default="Beginner")
+    platform_status = models.CharField(choices=PLATFORM_STATUS, default="Published")
+    teacher_course_status = models.CharField(choices=TEACHER_STATUS, default="Published")
+    featured = models.BooleanField(default=False)
+    course_id = ShortUUIDField(unique=True, length=6, max_length=20, alphabet="1234567890")
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):# rewriting default save method
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
+    def students(self):
+        return EnrolledCourse.objects.filter(course=self)
+    
+    def curriculum(self):
+        return VariantItem.objects.filter(variant__course=self)
+    
+    def lectures(self):
+        # Get the variant related to the VariantItem. and Then get the course associated with that variant.
+        return VariantItem.objects.filter(variant__course=self)
+    
+    def average_rating(self):
+        # The aggregate() function is used to compute summary statistics over the filtered queryset.
+        # models.Avg('rating') is a Django aggregation function that calculates the average value of the rating field for all reviews returned by the filter
+        # The result will be a dictionary where the key is avg_rating, and the value is the calculated average. {'avg_rating': 4.0}
+        average_rating = Review.objects.filter(course=self, active=True).aggregate(avg_rating=models.Avg('rating'))
+
+    def rating_count(self):
+        return Review.objects.filter(course=self, active=True).count()
+    
+    def reviews(self):
+        return Review.objects.filter(course=self, active=True)
+    
+
+class Variant(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=1000)
+    variant_id = ShortUUIDField(unique=True, Length=6, max_length=20, alphabet="1234567890")
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+    
+    def variant_items(self):
+        return VariantItem.objects.filter(variant=self)
+    
+class VariantItem(models.Model):
+    variant = models.ForeignKey(Variant, on_delete=models.CASCADE, related_name="variant_items")
+    title = models.CharField(max_length=1000)
+    description = models.TextField(null=True, blank=True)
+    file = models.FileField(upload_to="course-file")
+    duration = models.DurationField(null=True, blank=True)
+    content_duration = models.CharField(max_length=1000, null=True, blank=True)
+    preview = models.BooleanField(default=False)
+    variant_item_id = ShortUUIDField(unique=True, Length=6, max_length=20, alphabet="1234567890")
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.variant.title} - {self.title}"
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.file:
+            clip = VideoFileClip(self.file.path)
+            duration_seconds = clip.duration
+            
+            # divmod is python inbuilt funciton that two numbers as arguments and return tuple containing two values
+            minutes, reminder = divmod(duration_seconds, 60) # eg. 3600/60 = 60 is minutes, 0 is reminder
+            minutes = math.floor(minutes)
+            seconds = math.floor(reminder)
+
+            duration_text = f"{minutes}m {seconds}s" #60m 35s
+            self.content_duration = duration_text
+
+            # Here we saying updated field 'content_duration' update during save
+            super().save(update_fields=['content_duration'])
